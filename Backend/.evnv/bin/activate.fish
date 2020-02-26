3ba4a634825f2f3f87dbcd53d4165c1e089edce1
Nandan1567 <nandananandan7@gmail.com>
2020-02-26 13:24:12 +0530

    requirement.text file added

--- Backend/.evnv/bin/activate.fish
+++ Backend/.evnv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nandana/Restaurants/Backend/.evnv"
+set -gx VIRTUAL_ENV "/home/nandan/Restaurants/Backend/.evnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- Backend/.evnv/bin/django-admin.py
+++ Backend/.evnv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/nandana/Restaurants/Backend/.evnv/bin/python3
+#!/home/nandan/Restaurants/Backend/.evnv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
